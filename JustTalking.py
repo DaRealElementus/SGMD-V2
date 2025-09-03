16efc192f3ac4817ab7c268541c38bc796f1d15d
theOnlyHayBale <147785291+theOnlyHayBale@users.noreply.github.com>
2025-09-03 16:52:45 +1000

    Update JustTalking.py

--- JustTalking.py
+++ JustTalking.py
@@ -4,7 +4,7 @@ import simpleaudio as sa
 
 voice = PiperVoice.load("/home/rylan/Documents/Sigmund/Voice/English/en_GB-alan-medium.onnx")
 with wave.open("test.wav", "wb") as wav_file:
-    voice.synthesize_wav("Alex, of 7 Buzo GDNS Franlink ACT 2913 accept my love or perish", wav_file)
+    voice.synthesize_wav("words go here", wav_file)
 
 wave_obj = sa.WaveObject.from_wave_file("test.wav")
 play_obj = wave_obj.play()
