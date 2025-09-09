0bc94f9c9d197bf9a8b6234caab8f5f76f56122e
Elementus <145308756+DaRealElementus@users.noreply.github.com>
2025-09-09 14:45:39 +1000

    Post Presentation changes
    Removed Old files
     - auth.py
     - history_viewer.py
     - llama_chatbot_main.py
     - response_generation.py
    
    llama_wrapper.py
    + Increased Max Tokens
    + Added some debug scripting
    
    Requirements.txt
    + corrected the gputil lib

--- ChatGUI.py
+++ ChatGUI.py
@@ -1,8 +1,15 @@
+<<<<<<< Updated upstream
 import dearpygui.dearpygui as dpg  # Import DearPyGui for GUI creation
 import Client                      # Import custom Client module for encryption/decryption
 import requests                    # Import requests for HTTP communication
 
 SERVER = "http://121.45.50.210:5000/"  # Server URL for chat backend
+=======
+import dearpygui.dearpygui as dpg
+import Client
+import requests
+SERVER = Client.SERVER
+>>>>>>> Stashed changes
 
 def send_message_callback(sender, app_data, user_data):
     message = dpg.get_value("message_input")  # Get the message from input field
