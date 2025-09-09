ea8fd5dd6375cccd31a8f8a65dc99a6d073e9155
沒人知道 <147779565+Ogbeni28@users.noreply.github.com>
2025-09-09 19:11:06 +1000

    Rylan annoys me

--- JustTalking.py
+++ JustTalking.py
@@ -1,7 +1,7 @@
 import wave
 from piper import PiperVoice
 import simpleaudio as sa
-
+def justTalking(test):
 voice = PiperVoice.load("/home/rylan/Documents/Sigmund/Voice/English/en_GB-alan-medium.onnx")
 with wave.open("test.wav", "wb") as wav_file:
     voice.synthesize_wav("Hi! I'm Sigmund a local AI therapist, here to talk and aid you with any and every problem you may have.", wav_file)
